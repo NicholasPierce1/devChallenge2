28d12a797357f317cd96a2920fac1cbfff188596
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2020-03-02 10:26:59 -0600

    amends function to split chars, not words

--- scripts.js
+++ scripts.js
@@ -10,16 +10,16 @@ userInput.addEventListener("blur", async () => {
     
     console.log('event fired');
     // extracts text from user and stores its length
-    const wordsEntered = userInput.value.split(' ').length;
+    const wordsEntered = userInput.value.split('').length;
 
     
     // stores output in label
-   ouputLabel.innerHTML = `Words Left: ${maxStringLength - wordsEntered}`;
+   ouputLabel.innerHTML = `<b>Characters Left: ${maxStringLength - wordsEntered}</b>`;
 });
 
 window.addEventListener("load", async () => {
     
     // inits label output
-    ouputLabel.innerHTML = "Worlds Left: 250";
+    ouputLabel.innerHTML = "<b>Characters Left: 250</b>";
     
 });
